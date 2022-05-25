59392b2ea8c7f58c94b99be9238d964f81096358
buzeqq <miloszchoj@gmail.com>
2022-05-25 20:20:26 +0200

    Key modification, image preview

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-from crypto_functions import *
+import os
+import random
+import sys
 
 
 def main():
-    file = open('/home/buzeqq/Dokumenty/test.txt', 'r+b')
-    data = bytearray(file.read())
-    file.close()
+    if len(sys.argv) < 2:
+        print('Please specify key path!')
+        return
 
-    print('ECB:')
-    output = encrypt('AES', 'ECB', 32, data)
-    print('key:', output['key'])
-    print('ciphertext:', output['ciphertext'])
-    data = decrypt('AES', 'ECB', output['key'], output['additional'], output['ciphertext'])
-    print(f'data: {data}\n')
+    path = sys.argv[1]
+    with open(path, 'rb') as key, open(os.path.dirname(path) + "/new_key.bin", 'wb') as new_key:
+        key = key.read()
+        print(key, len(key))
+        new_key_bytes = []
+        for byte in key:
+            if random.random() < 0.3:
+                byte = byte ^ int.from_bytes(os.urandom(1), 'big')
+            new_key_bytes.append(byte)
+        new_key_bytes = bytes(new_key_bytes)
+        print(new_key_bytes, len(key))
+        new_key.write(new_key_bytes)
 
-    print('CBC:')
-    output = encrypt('AES', 'CBC', 32, data)
-    print('key:', output['key'])
-    print('initialization vector:', output['additional'])
-    print('ciphertext:', output['ciphertext'])
-    data = decrypt('AES', 'CBC', output['key'], output['additional'], output['ciphertext'])
-    print(f'data: {data}\n')
-
-    print('CTR:')
-    output = encrypt('AES', 'CTR', 32, data)
-    print('key:', output['key'])
-    print('nonce:', output['additional'])
-    print('ciphertext:', output['ciphertext'])
-    data = decrypt('AES', 'CTR', output['key'], output['additional'], output['ciphertext'])
-    print(f'data: {data}\n')
 
 if __name__ == '__main__':
     main()
